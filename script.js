e08f5cb4def842466ec0c162444b7862ef2752c1
Guilherme Bittencourt Ramos <166452733+guilhermeramosbr@users.noreply.github.com>
2024-04-08 21:59:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let resulado = document.getElementById('divResultado')
+// console.log(resulado);
+//  resulado.innerHTML = "ola, eu sou uma div legal :*"
+
+
+// let cont = 0
+// while(cont < 10){
+//      console.log(cont)
+//     resulado.innerHTML += cont + "patinho foi passear ... <br>"
+// cont++
+
+// }
+// let cont = 0, soma
+// while(cont<=10){
+//     soma = cont ** 2
+//     resulado.innerHTML += soma + '<br>'
+//     cont++
+    
+// }
+
+// let numer, cont= 10
+// while(cont >0){
+//     resulado.innerHTML += cont + '<br>'
+//     cont--
+// }
+ 
+// 
+
+// let cont = 0, soma
+// while(cont <= 10){
+//     soma = cont *5
+//     resulado.innerHTML += soma + '<br>'
+//     cont++
+// }
+
+// let cont = 0 , soma
+// let numero = Number(prompt('dite um numero'))
+// while(cont <= 10 ){
+//     soma = numero * cont
+//     resulado.innerHTML += soma + '<br>'
+// cont++
+// }
